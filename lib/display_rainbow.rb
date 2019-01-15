22286ca4c553046c6819ffae74e04875a1615450
Mubeen Ghauri <ghauri.mubeen@gmail.com>
2019-01-15 10:43:05 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
 # Write your #display_rainbow method here
+
+
+def display_rainbow(list)
+
+  e = ""    ### empty string
+  ls = Array.new
+  for i in list
+    e = "#{i[0].upcase} : #{i}"
+    ls.push e
+  end
+  puts ls
+end
+
+
+display_rainbow(['red','green'])
